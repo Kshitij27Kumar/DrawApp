a3a7b740bbe06a39743f8bda7fad4028e0ae3bb1
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-12 22:17:12 +0530

    Color change, clear canvas and few fixes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const colorEl = document.getElementById('color')
 const clearEl = document.getElementById('clear')
 const ctx = canvas.getContext('2d')
 
-let size = 35
+let size = 25
 let isPressed = false
 let color = 'black'
 let x = undefined
@@ -28,8 +28,9 @@ canvas.addEventListener('mousemove', (e) => {
   if (isPressed) {
     const x1 = e.offsetX
     const y1 = e.offsetY
-    drawLine(x, y, x1, y1)
     drawCircle(x1, y1)
+    drawLine(x, y, x1, y1)
+
     x = x1
     y = y1
   }
@@ -37,7 +38,7 @@ canvas.addEventListener('mousemove', (e) => {
 
 drawCircle = (x, y) => {
   ctx.beginPath()
-  ctx.arc(x, y, size, 0, MATH * PI * 2)
+  ctx.arc(x, y, size, 0, Math.PI * 2)
   ctx.fillStyle = color
   ctx.fill()
 }
@@ -62,3 +63,16 @@ decrementBtn.addEventListener('click', () => {
   if (size < 5) size = 5
   updateSizeOnScreen()
 })
+
+colorEl.addEventListener('change', (e) => {
+  color = e.target.value
+  console.log(color)
+})
+
+clearEl.addEventListener('click', (e) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+updateSizeOnScreen = () => {
+  sizeEl.innerText = size
+}
